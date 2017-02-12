833192ac4e4ae0b6244cf3dd1bcafc44428e26f7
vrathsmill <vlrathsmill@gmail.com>
2017-02-11 21:11:54 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ("Mr. Chocolate", 3, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 1, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 6,"F");
+INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown", 10, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 2, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Grinch", 2,"M");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 2, "F");
+INSERT INTO bears (name, age, gender) VALUES (Null, 2, "M");
